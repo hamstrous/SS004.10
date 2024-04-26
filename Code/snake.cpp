9b9a716973efd4aa58bbdab8b9251b9bbc8a38a0
23520561 <23520561@gm.uit.edu.vn>
2024-04-26 10:22:33 +0700

    Create game loop

--- Code/snake.cpp
+++ Code/snake.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <raylib.h>
 
 using namespace std;
 
 int main(){
-  cout << "s" << endl;
+  cout << "Game starting ..." << endl;
+  InitWindow(750, 750, "Snake");
+  SetTargetFPS(60);
+
+  while(WindowShouldClose() == false)
+  {
+    BeginDrawing();
+    //Draw
+    ClearBackground(GREEN);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
