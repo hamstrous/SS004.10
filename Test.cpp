cd4d093c8ba53855e3718f168058d569eed08e7c
23521006 <168098967+23521006@users.noreply.github.com>
2024-05-24 23:57:51 +0700

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 static bool allowMove = false;
-Color green = { 173, 204, 96, 255 };
+Color green = { 6, 191, 43, 255 };
+Color skyblue = { 157, 215, 239, 255 };
 Color darkGreen = { 43, 51, 24, 255 };
 Color white = {255, 255, 255, 255};
 //kích thước mỗi ô trên bản đồ (pixel)
@@ -57,7 +58,40 @@ public:
             float x = body[i].x;
             float y = body[i].y;
             Rectangle segment = Rectangle{ offset + x * cellSize, offset + y * cellSize, (float)cellSize, (float)cellSize };
-            DrawRectangleRounded(segment, 0.5, 6, darkGreen);
+            DrawRectangleRounded(segment, 0.5, 6, green);
+        }
+
+        if (body[0].x > body[1].x)
+        {
+            Texture2D head_texture;
+            Image head_image = LoadImage("Graphics/RightHead.png");
+            head_texture = LoadTextureFromImage(head_image);
+            UnloadImage(head_image);
+            DrawTexture(head_texture, offset + body[0].x * cellSize, offset + body[0].y * cellSize - 1, WHITE);
+        }
+        if (body[0].x < body[1].x)
+        {
+            Texture2D head_texture;
+            Image head_image = LoadImage("Graphics/LeftHead.png");
+            head_texture = LoadTextureFromImage(head_image);
+            UnloadImage(head_image);
+            DrawTexture(head_texture, offset + body[0].x * cellSize - 9, offset + body[0].y * cellSize - 1, WHITE);
+        }
+        if (body[0].y < body[1].y)
+        {
+            Texture2D head_texture;
+            Image head_image = LoadImage("Graphics/UpHead.png");
+            head_texture = LoadTextureFromImage(head_image);
+            UnloadImage(head_image);
+            DrawTexture(head_texture, offset + body[0].x * cellSize - 1, offset + body[0].y * cellSize - 9, WHITE);
+        }
+        if (body[0].y > body[1].y)
+        {
+            Texture2D head_texture;
+            Image head_image = LoadImage("Graphics/DownHead.png");
+            head_texture = LoadTextureFromImage(head_image);
+            UnloadImage(head_image);
+            DrawTexture(head_texture, offset + body[0].x * cellSize - 1, offset + body[0].y * cellSize, WHITE);
         }
     }
 
@@ -213,14 +247,14 @@ public:
 int main()
 {
     cout << "Starting the game..." << endl;
-    InitWindow(2 * offset + cellSize * cellCount, 2 * offset + cellSize * cellCount, "Retro Snake");
+    InitWindow(2 * offset + cellSize * cellCount, 2 * offset + cellSize * cellCount, "Snake Game");
     SetTargetFPS(60);
 
     Game game = Game();
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
-        ClearBackground(green);
+        ClearBackground(skyblue);
         DrawRectangleLinesEx(Rectangle{ (float)(offset + cellSize * 5), (float)(offset + cellSize * 13), (float)cellSize * 15, (float)cellSize * 3 }, 5, darkGreen);
         DrawText("Snake", (float)(offset + cellSize * 5) + cellSize * 3, (float)(offset + cellSize * 5), 90, darkGreen);
         DrawText("Start", (float)(offset + cellSize * 5) + cellSize * 5, (float)(offset + cellSize * 13) + 20, 50, darkGreen);
@@ -239,6 +273,11 @@ int main()
         }
         while (start)
         {
+            Texture2D texture;
+            Image image = LoadImage("Graphics/food.png");
+            texture = LoadTextureFromImage(image);
+            UnloadImage(image);
+            DrawTexture(texture, offset - 5, offset + cellSize * cellCount + 12, WHITE);
             BeginDrawing();
             if (EventTriggered(0.2))
             {
@@ -267,10 +306,10 @@ int main()
             }
 
             // Drawing
-            ClearBackground(green);
+            ClearBackground(skyblue);
             DrawRectangleLinesEx(Rectangle{ (float)offset - 5, (float)offset - 5, (float)cellSize * cellCount + 10, (float)cellSize * cellCount + 10 }, 5, darkGreen);
             DrawText("Snake", offset - 5, 20, 40, darkGreen);
-            DrawText(TextFormat("%i", game.score), offset - 5, offset + cellSize * cellCount + 10, 40, darkGreen);
+            DrawText(TextFormat("%i", game.score), offset + 28, offset + cellSize * cellCount + 10, 40, darkGreen);
             game.Draw();
             EndDrawing();
             if (WindowShouldClose())
